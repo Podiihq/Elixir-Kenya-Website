878b86a63cc641c4f6caac1c0d9bf589eee1dd5b
Parlgy <parlg4776@gmail.com>
2023-08-29 14:19:51 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const navToggle = document.getElementById("nav-toggle");
+const mobileNav = document.getElementById("mobile-nav");
+
+navToggle.addEventListener("click", () => {
+  mobileNav.classList.toggle("hidden");
+});
